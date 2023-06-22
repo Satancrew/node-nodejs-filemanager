1948e6c45174f2ea80d97f463b074c6a766ec4a4
Vladimir Orlov <satancrew.true@gmail.com>
2023-06-22 17:38:48 +0300

    feat: add os cpus task solution

--- src/os/os.js
+++ src/os/os.js
@@ -1,11 +1,27 @@
 import os from 'os';
 import { wrongOperation } from '../helpers/utils/wrongOperation.js';
+import { greenText } from '../helpers/coloredText/greenText.js';
 
 export const osParams = async param => {
   switch (param) {
     case '--EOL':
       try {
         console.log(`\n${JSON.stringify(os.EOL)}`);
+      } catch (error) {
+        wrongOperation('fail');
+      }
+      break;
+    case '--cpus':
+      try {
+        for (let cpu of os.cpus()) {
+          console.log(
+            `Model: `,
+            greenText(cpu.model),
+            ` Speed: `,
+            greenText(cpu.speed),
+          );
+        }
+        console.log(`\nAmount of CPU: ${greenText(os.cpus().length)}\n`);
       } catch (error) {
         wrongOperation('fail');
       }
